02e49814a4a99b3611e1530bd19a3155c0c384ac
chathura-ipos <chathura.iposg@gmail.com>
2022-01-21 16:59:57 +0530

    asd

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <button class="bg-cyan-500 shadow-lg shadow-cyan-500/50">
+        Subscribe
+      </button>
+      <button class="bg-blue-500 shadow-lg shadow-blue-500/50">
+        Subscribe
+      </button>
+      <button class="bg-indigo-500 shadow-lg shadow-indigo-500/50">
+        Subscribe
+      </button>
+    </div>
+  );
 }
 
 export default App;
